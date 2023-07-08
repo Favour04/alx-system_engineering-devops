a2b50186fde10770759c19b4b6aec0ad32ca4e1f
Favour04 <idaeworfavour1@gmail.com>
2023-07-08 20:55:44 +0100

    The complexity of the algorithm is O(nlog(n))

---
+++ 0x07-networking_basics/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    This module is
+    contain the func
+    find_peak that is
+    defined to find
+    the peak in a li-
+    st of numbers
+"""
+
+
+def find_peak(list_of_numbers):
+    if len(list_of_numbers) == 0:
+        return None
+    else:
+        peak = sorted(list_of_numbers)
+        return peak[-1]
