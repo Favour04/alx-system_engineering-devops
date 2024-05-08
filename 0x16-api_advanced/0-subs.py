85fa0bc3a887fe2ca1e81e078e6d28fc61dc1a87
favour04 <idaeworfavour1@gmail.com>
2024-05-08 03:40:12 +0100

    i am back

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,20 +14,20 @@ def number_of_subscribers(subreddit):
     """
     req = urllib.request.Request('https://reddit.com/r/' +
                                  f'{subreddit}' + '/about.json')
+    try:
+        with urllib.request.urlopen(req) as response:
+            status_code = response.getcode()
 
-    with urllib.request.urlopen(req) as response:
-        status_code = response.getcode()
+            if status_code == 200:
+                fetched_data = response.read()
+                fetched_data = fetched_data.decode('utf-8')
+                fetched_data = json.loads(fetched_data)
 
-        if status_code == 200:
-            fetched_data = response.read()
-            fetched_data = fetched_data.decode('utf-8')
-            fetched_data = json.loads(fetched_data)
-
-            if 'data' in fetched_data:
-                if 'subscribers' in fetched_data['data']:
-                    count = fetched_data['data']['subscribers']
-                    return count
-        else:
-            return 0
-
-    return 0
+                if 'data' in fetched_data:
+                    if 'subscribers' in fetched_data['data']:
+                        count = fetched_data['data']['subscribers']
+                        return count
+            else:
+                return 0
+    except Exception as e:
+        return 0
